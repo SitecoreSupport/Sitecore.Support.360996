6e614181ecca48b7b3b2ee4a07ec67635e5a338b
Paul Stupka <paul.stupka@sitecore.com>
2019-09-26 16:11:59 +0300

    clean patch

---
+++ src/Sitecore.Support.360996/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.360996")]
+[assembly: AssemblyProduct("Sitecore.Support.360996")]
+[assembly: ComVisible(false)]
