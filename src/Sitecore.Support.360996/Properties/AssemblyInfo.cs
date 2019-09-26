cb806a685c91b81dbc7752eca982ab6220a9c1cc
Paul Stupka <paul.stupka@sitecore.com>
2019-09-26 16:58:41 +0300

    base implementation

--- src/Sitecore.Support.360996/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.360996/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.360996")]
 [assembly: AssemblyProduct("Sitecore.Support.360996")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Avoid fast queries when using the Content Token SXA feature")]
+
---
+++ src/Sitecore.Support.360996/XA/Feature/ContentTokens/Pipelines/RenderField/RenderContentToken.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.XA.Feature.ContentTokens;
+
+namespace Sitecore.Support.XA.Feature.ContentTokens.Pipelines.RenderField
+{
+    public class RenderContentToken : Sitecore.XA.Feature.ContentTokens.Pipelines.RenderField.RenderContentToken
+    {
+        protected override string GetTextVariableValue(string variableKey)
+        {
+            if (!string.IsNullOrWhiteSpace(variableKey))
+            {
+                Item variable = Sitecore.Context.Database.SelectSingleItem($"fast://*[@@templateid='{Templates.ContentToken.ID}' and @#Key#='{variableKey}']");
+                if (variable != null)
+                {
+                    return variable[Templates.ContentToken.Fields.Value];
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
